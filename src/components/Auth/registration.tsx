53998d38f51daa98ca84a930881e97976dc83496
Sally-N <snzungula@gmail.com>
2023-07-09 14:56:07 +0300

    api fix reg

--- src/components/Auth/registration.tsx
+++ src/components/Auth/registration.tsx
@@ -10,18 +10,13 @@ const SignupComponent = () => {
     const [cpassword, setConfirmPassword] = useState('');
     const [username, setUserName] = useState('')
     const [email, setEmail] = useState('')
+    const [result, setResult] = useState<any>();
     const [confirmPasswordError, setConfirmPasswordError] = useState('');
     const baseUrl = "https://www.muganedev.tech/api/v1/"
 
-    const handleSubmit = (formSubmit: FormEvent<HTMLFormElement>) => {
-       
-        const formData = {
-            username,
-            email,
-            password,
-        };
-        console.log(formData)
-        formSubmit.preventDefault()
+    const handleSubmit = async (submit: FormEvent<HTMLFormElement>) => {
+
+        submit.preventDefault()
         setConfirmPasswordError('');
 
         if (password !== cpassword) {
@@ -29,64 +24,73 @@ const SignupComponent = () => {
             return;
         }
 
-        axios.post(`${baseUrl}users/create`, formData, { auth: {
-            username: "snzungula@gmail.com",
-            password: "foundation25"
-          }})
-            .then(res => {
-                setbusy('Loading')
-                localStorage.setItem('user', JSON.stringify(formData));
+        const formData = {username, email, password}
 
-                window.location.href = "/login"
-                console.log(res.data);
-            })
-            .catch(err => {
-                console.log(err)
-                setbusy('')
-            })
-    }
+        // const formData = new FormData();
+        // formData.append('username', username);
+        // formData.append('email', email);
+        // formData.append('password', password);
+        // console.log(formData, "formdata")
+        console.log(formData, 'fd')
+
+        try {
+            const response = fetch('/api/signup', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData)
+            });
+            localStorage.setItem('user', JSON.stringify(formData));
+            console.log(response, "data");
+            window.location.href = "/login";
+        } catch (error) {
+            console.log(error);
+        }
+    };
+        
     return <section className="ftco-section">
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-7 col-lg-5">
-                    <div className="wrap mt-4">
-                        <div className="login-wrap mt-4 p-4 p-md-5">
-                            <div className="d-flex">
-                                <div className="w-100">
-                                    <h3 className="mb-2 text-center">Create an Account</h3>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-7 col-lg-5">
+                        <div className="wrap mt-4">
+                            <div className="login-wrap mt-4 p-4 p-md-5">
+                                <div className="d-flex">
+                                    <div className="w-100">
+                                        <h3 className="mb-2 text-center">Create an Account</h3>
+                                    </div>
                                 </div>
+                                <form onSubmit={handleSubmit} className="signin-form" action="#">
+                                    <div className="form-group mt-4">
+                                        <input type="text" className="form-control rounded-pill input-area" placeholder="Username"
+                                            onChange={(e) => setUserName(e.target.value)} required />
+                                    </div>
+                                    <div className="form-group mt-4">
+                                        <input type="text" className="form-control rounded-pill input-area" placeholder="Email address"
+                                            onChange={(e) => setEmail(e.target.value)} required />
+                                    </div>
+                                    <div className="form-group mt-4">
+                                        <input id="password-field" type="password" value={password} className="form-control rounded-pill input-area" placeholder="Password"
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required />
+                                    </div>
+                                    <div className="form-group mt-4">
+                                        <input id="cpassword-field" type="password" value={cpassword} className="form-control rounded-pill input-area" placeholder="Confirm Password"
+                                            onChange={(e) => setConfirmPassword(e.target.value)}
+                                            required />
+                                    </div>
+                                    <div className="form-group mt-4">
+                                        <button type="submit" className="form-control btn btn-primary btn-teal rounded-pill submit px-3">Sign Up</button>
+                                    </div>
+                                </form>
+                                <p className="text-center mt-4">Already have an account? <Link data-toggle="tab" href="/login" className="fw-bold txt-link">Sign In</Link></p>
                             </div>
-                            <form action="#" className="signin-form" onSubmit={handleSubmit}>
-                                <div className="form-group mt-4">
-                                    <input type="text" className="form-control rounded-pill input-area" placeholder="Username" 
-                                     onChange={(e) => setUserName(e.target.value)}required />
-                                </div>
-                                <div className="form-group mt-4">
-                                    <input type="text" className="form-control rounded-pill input-area" placeholder="Email address"
-                                     onChange={(e) => setEmail(e.target.value)} required />
-                                </div>
-                                <div className="form-group mt-4">
-                                    <input id="password-field" type="password" value={password} className="form-control rounded-pill input-area" placeholder="Password"
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        required />
-                                </div>
-                                <div className="form-group mt-4">
-                                    <input id="password-field" type="password" value={cpassword} className="form-control rounded-pill input-area" placeholder="Confirm Password"
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                        required />
-                                </div>
-                                <div className="form-group mt-4">
-                                    <button type="submit" className="form-control btn btn-primary btn-teal rounded-pill submit px-3">Sign Up</button>
-                                </div>
-                            </form>
-                            <p className="text-center mt-4">Already have an account? <Link data-toggle="tab" href="/login" className="fw-bold txt-link">Sign In</Link></p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-}
+    }
 
-export default SignupComponent;
+    export default SignupComponent;
