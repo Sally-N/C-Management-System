14b5233f08c41e2a7fdeb85de8410d0682a5e3f0
Sally-N <snzungula@gmail.com>
2023-07-14 11:21:11 +0300

    post requests update

--- pages/api/updateprofile.ts
+++ pages/api/updateprofile.ts
@@ -1,9 +1,9 @@
 import { baseUrl } from "../../src/components/home";
 
 
-export default async function handler(req: { body: {mp: any; auth: any; uId: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }) {
+export default async function handler(req: { body: { mp: any; auth: any; uId: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }) {
     const { mp, auth, uId } = req.body;
-    const {username, password} = auth
+    const { username, password } = auth
 
     try {
         const response = await fetch(`${baseUrl}users/update/${uId}`, {
@@ -12,12 +12,12 @@ export default async function handler(req: { body: {mp: any; auth: any; uId: any
                 'Content-Type': 'application/json',
                 'Authorization': `Basic ${btoa(`${username}:${password}`)}`
             },
-            body: JSON.stringify({ mp }), 
-           });
-              const data = await response.json();
-   console.log('====================================');
-   console.log(res.status(200).json(data), 'updateprofile page');
-   console.log('====================================');        
+            body: JSON.stringify({ mp }),
+        });
+        const data = await response.json();
+        console.log('====================================');
+        console.log(res.status(200).json(data), 'updateprofile page');
+        console.log('====================================');
     } catch (error) {
         res.status(500).json({ message: 'Error updating profile' });
     }
